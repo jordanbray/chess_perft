043bc4d04ab74e9cd9e0bef4dd98e048c46bf21a
Jordan Bray <jordanbray@gmail.com>
2016-05-28 14:11:32 -0400

    Added cache table size.

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use chess::board::Board;
 use std::time::SystemTime;
 use std::env;
 
-fn perform_perft(fen: String, depth: u64) {
+fn perform_perft(fen: String, depth: u64, cache_size: usize) {
     let board = Board::from_fen(fen.to_owned());
     let start = SystemTime::now();
-    let result = board.perft(depth);
+    let result = if cache_size == 0 { board.perft(depth) } else { board.perft_cache(depth, cache_size) };
     let duration = SystemTime::now().duration_since(start);
     match duration {
         Ok(clock) => {
@@ -31,10 +31,12 @@ fn main() {
 
     let mut fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_owned();
     let mut depth = 6;
+    let mut cache = 0;
 
     let mut opts = Options::new();
     opts.optopt("f", "fen", "set the FEN to perfromt the perft on.", "FEN");
     opts.optopt("d", "depth", "set the depth to process the perft.", "DEPTH");
+    opts.optopt("c", "cache", "set the cache size to a particular value.  Use 0 for no cache.", "CACHE_SIZE");
     opts.optflag("h", "help", "print this help menu");
 
     let matches = match opts.parse(&args[1..]) {
@@ -57,7 +59,12 @@ fn main() {
         Some(x) => depth = x.parse::<u64>().unwrap()
     }
 
+    match matches.opt_str("c") {
+        None => {},
+        Some(x) => cache = x.parse::<usize>().unwrap()
+    }
+
     chess::construct::construct();
 
-    perform_perft(fen, depth);
+    perform_perft(fen, depth, cache);
 }
