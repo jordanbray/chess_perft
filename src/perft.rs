a5eae46b62beb5509407cb7ae4f6170495982b99
analog-hors <44050761+analog-hors@users.noreply.github.com>
2021-11-18 19:09:20 +1000

    Switch from SystemTime to Instant, fix a few problems

--- src/perft.rs
+++ src/perft.rs
@@ -1,50 +1,36 @@
 use chess::{Board, MoveGen};
 use shakmaty::{fen::Fen, perft as sperft, Chess};
 use std::str::FromStr;
-use std::time::SystemTime;
+use std::time::Instant;
 
 pub fn perform_perft(fen: String, depth: u64, chess: bool, shakmaty: bool) {
     if chess {
         let board = Board::from_str(&fen).unwrap();
-        let start = SystemTime::now();
+        let start = Instant::now();
         let result = MoveGen::movegen_perft_test(&board, depth as usize) as u64;
-        let duration = SystemTime::now().duration_since(start);
-        match duration {
-            Ok(clock) => {
-                println!(
-                    "chess   : Perft {} of {}\tResult: {}\tTime: {}s {}ms",
-                    depth,
-                    fen,
-                    result,
-                    clock.as_secs(),
-                    clock.subsec_nanos() / 1000000
-                );
-            }
-            Err(_) => {
-                panic!();
-            }
-        }
+        let duration = start.elapsed();
+        println!(
+            "chess   : Perft {} of {}\tResult: {}\tTime: {}s {}ms",
+            depth,
+            fen,
+            result,
+            duration.as_secs(),
+            duration.subsec_nanos() / 1000000
+        );
     }
     if shakmaty {
         let setup: Fen = fen.parse().expect("invaild fen");
         let position: Chess = setup.position().expect("invalid setup");
-        let start = SystemTime::now();
+        let start = Instant::now();
         let result = sperft(&position, depth as u32);
-        let duration = SystemTime::now().duration_since(start);
-        match duration {
-            Ok(clock) => {
-                println!(
-                    "shakmaty: Perft {} of {}\tResult: {}\tTime: {}s {}ms",
-                    depth,
-                    fen,
-                    result,
-                    clock.as_secs(),
-                    clock.subsec_nanos() / 1000000
-                );
-            }
-            Err(_) => {
-                panic!();
-            }
-        }
+        let duration = start.elapsed();
+        println!(
+            "shakmaty: Perft {} of {}\tResult: {}\tTime: {}s {}ms",
+            depth,
+            fen,
+            result,
+            duration.as_secs(),
+            duration.subsec_nanos() / 1000000
+        );
     }
 }
