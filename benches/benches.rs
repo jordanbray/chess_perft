735574a9b4f152f0750fb2e49c3c0d94e28b7fd4
Nicolas Bird von Eyben <nicolas.bird.voneyben@gmail.com>
2025-03-19 13:52:12 -0700

    up

--- benches/benches.rs
+++ benches/benches.rs
@@ -17,6 +17,17 @@ fn movegen_perft(bench: &mut Bencher, fen: String, depth: usize, count: usize) {
     bench.iter(|| assert_eq!(MoveGen::movegen_perft_test(&pos, depth), count));
 }
 
+fn nightly_movegen_perft(bench: &mut Bencher, fen: String, depth: usize, count: usize) {
+    let pos = chess_nightly::Board::from_str(&fen).expect("Valid FEN");
+
+    bench.iter(|| {
+        assert_eq!(
+            chess_nightly::MoveGen::movegen_perft_test(&pos, depth),
+            count
+        )
+    });
+}
+
 fn shakmaty_perft(bench: &mut Bencher, fen: String, depth: u64, count: u64) {
     let pos: Chess = fen
         .parse::<Fen>()
@@ -288,6 +299,220 @@ fn perft_25_chess_incremental(bench: &mut Bencher) {
 
 fn perft_26_chess_incremental(bench: &mut Bencher) {
     movegen_perft(
+        bench,
+        "8/5k2/8/5N2/5Q2/2K5/8/8 w - - 0 1".to_owned(),
+        4,
+        23527,
+    );
+}
+
+fn perft_01_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "8/5bk1/8/2Pp4/8/1K6/8/8 w - d6 0 1".to_owned(),
+        6,
+        824064,
+    ); // Invalid FEN
+}
+
+fn perft_02_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "8/8/1k6/8/2pP4/8/5BK1/8 b - d3 0 1".to_owned(),
+        6,
+        824064,
+    ); // Invalid FEN
+}
+
+fn perft_03_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1".to_owned(),
+        6,
+        1440467,
+    );
+}
+
+fn perft_04_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "8/5k2/8/2Pp4/2B5/1K6/8/8 w - d6 0 1".to_owned(),
+        6,
+        1440467,
+    );
+}
+
+fn perft_05_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "5k2/8/8/8/8/8/8/4K2R w K - 0 1".to_owned(),
+        6,
+        661072,
+    );
+}
+
+fn perft_06_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "4k2r/8/8/8/8/8/8/5K2 b k - 0 1".to_owned(),
+        6,
+        661072,
+    );
+}
+
+fn perft_07_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "3k4/8/8/8/8/8/8/R3K3 w Q - 0 1".to_owned(),
+        6,
+        803711,
+    );
+}
+
+fn perft_08_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "r3k3/8/8/8/8/8/8/3K4 b q - 0 1".to_owned(),
+        6,
+        803711,
+    );
+}
+
+fn perft_09_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1".to_owned(),
+        4,
+        1274206,
+    );
+}
+
+fn perft_10_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "r3k2r/7b/8/8/8/8/1B4BQ/R3K2R b KQkq - 0 1".to_owned(),
+        4,
+        1274206,
+    );
+}
+
+fn perft_11_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1".to_owned(),
+        4,
+        1720476,
+    );
+}
+
+fn perft_12_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "r3k2r/8/5Q2/8/8/3q4/8/R3K2R w KQkq - 0 1".to_owned(),
+        4,
+        1720476,
+    );
+}
+
+fn perft_13_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1".to_owned(),
+        6,
+        3821001,
+    );
+}
+
+fn perft_14_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "3K4/8/8/8/8/8/4p3/2k2R2 b - - 0 1".to_owned(),
+        6,
+        3821001,
+    );
+}
+
+fn perft_15_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1".to_owned(),
+        5,
+        1004658,
+    );
+}
+
+fn perft_16_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "5K2/8/1Q6/2N5/8/1p2k3/8/8 w - - 0 1".to_owned(),
+        5,
+        1004658,
+    );
+}
+
+fn perft_17_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "4k3/1P6/8/8/8/8/K7/8 w - - 0 1".to_owned(),
+        6,
+        217342,
+    );
+}
+
+fn perft_18_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "8/k7/8/8/8/8/1p6/4K3 b - - 0 1".to_owned(),
+        6,
+        217342,
+    );
+}
+
+fn perft_19_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(bench, "8/P1k5/K7/8/8/8/8/8 w - - 0 1".to_owned(), 6, 92683);
+}
+
+fn perft_20_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(bench, "8/8/8/8/8/k7/p1K5/8 b - - 0 1".to_owned(), 6, 92683);
+}
+
+fn perft_21_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(bench, "K1k5/8/P7/8/8/8/8/8 w - - 0 1".to_owned(), 6, 2217);
+}
+
+fn perft_22_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(bench, "8/8/8/8/8/p7/8/k1K5 b - - 0 1".to_owned(), 6, 2217);
+}
+
+fn perft_23_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "8/k1P5/8/1K6/8/8/8/8 w - - 0 1".to_owned(),
+        7,
+        567584,
+    );
+}
+
+fn perft_24_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "8/8/8/8/1k6/8/K1p5/8 b - - 0 1".to_owned(),
+        7,
+        567584,
+    );
+}
+
+fn perft_25_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
+        bench,
+        "8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1".to_owned(),
+        4,
+        23527,
+    );
+}
+
+fn perft_26_chess_nightly_incremental(bench: &mut Bencher) {
+    nightly_movegen_perft(
         bench,
         "8/5k2/8/5N2/5Q2/2K5/8/8 w - - 0 1".to_owned(),
         4,
@@ -1085,6 +1310,32 @@ benchmark_group!(
     perft_24_chess_incremental,
     perft_25_chess_incremental,
     perft_26_chess_incremental,
+    perft_01_chess_nightly_incremental,
+    perft_02_chess_nightly_incremental,
+    perft_03_chess_nightly_incremental,
+    perft_04_chess_nightly_incremental,
+    perft_05_chess_nightly_incremental,
+    perft_06_chess_nightly_incremental,
+    perft_07_chess_nightly_incremental,
+    perft_08_chess_nightly_incremental,
+    perft_09_chess_nightly_incremental,
+    perft_10_chess_nightly_incremental,
+    perft_11_chess_nightly_incremental,
+    perft_12_chess_nightly_incremental,
+    perft_13_chess_nightly_incremental,
+    perft_14_chess_nightly_incremental,
+    perft_15_chess_nightly_incremental,
+    perft_16_chess_nightly_incremental,
+    perft_17_chess_nightly_incremental,
+    perft_18_chess_nightly_incremental,
+    perft_19_chess_nightly_incremental,
+    perft_20_chess_nightly_incremental,
+    perft_21_chess_nightly_incremental,
+    perft_22_chess_nightly_incremental,
+    perft_23_chess_nightly_incremental,
+    perft_24_chess_nightly_incremental,
+    perft_25_chess_nightly_incremental,
+    perft_26_chess_nightly_incremental,
     perft_kiwipete_chess_incremental,
 );
 
